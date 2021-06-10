e7ba4f90f059439ef0ed8e0532f0480d2c66b1cc
0b1kn00b <polysemantic@gmail.com>
2021-06-10 09:55:35 +0200

    flatten to fix type bug

--- src/main/haxe/stx/http/client/Fetch.hx
+++ src/main/haxe/stx/http/client/Fetch.hx
@@ -1,17 +1,15 @@
 package stx.http.client;
 
-abstract class Fetch<C:FetchConfigDef,P,R,E> implements ClientApi<P,R,E> extends Configured<C>{
+abstract class Fetch<C:FetchConfigDef,P,R,E> extends ClientCls<P,R,E>{
 
-  public final extractor : RemotingContextExtractor<R,E>;
-  
+  private final config    : C; 
+  public final extractor  : RemotingContextExtractor<R,E>;
+
   public function new(config:C,extractor:RemotingContextExtractor<R,E>){
-    super(config);
-    this.extractor = extractor;
+    this.config     = config;
+    this.extractor  = extractor;
   }
   public function request(method:HttpMethod,path:String,headers:Headers,body:Option<P>):Request{
     return Request.make(method,'${this.config.base}$path',this.config.options.headers.concat(headers),body.defv(null));
-  }
-  public function asClientDef():ClientDef<P,R,E>{
-    return cast this;//TODO bug?`gi
   }
 }
