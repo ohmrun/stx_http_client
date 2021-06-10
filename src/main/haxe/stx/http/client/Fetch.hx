2b81cd3b9a69521276a4676c075bb642dce319cd
0b1kn00b <polysemantic@gmail.com>
2021-06-10 09:50:55 +0200

    Fetch needs asClientDef#3

--- src/main/haxe/stx/http/client/Fetch.hx
+++ src/main/haxe/stx/http/client/Fetch.hx
@@ -4,7 +4,7 @@ abstract class Fetch<C:FetchConfigDef,P,R,E> implements ClientApi<P,R,E> extends
 
   public final extractor : RemotingContextExtractor<R,E>;
   
-  public function new(config,extractor){
+  public function new(config:C,extractor:RemotingContextExtractor<R,E>){
     super(config);
     this.extractor = extractor;
   }
@@ -12,6 +12,6 @@ abstract class Fetch<C:FetchConfigDef,P,R,E> implements ClientApi<P,R,E> extends
     return Request.make(method,'${this.config.base}$path',this.config.options.headers.concat(headers),body.defv(null));
   }
   public function asClientDef():ClientDef<P,R,E>{
-    return this;
+    return cast this;//TODO bug?`gi
   }
 }
