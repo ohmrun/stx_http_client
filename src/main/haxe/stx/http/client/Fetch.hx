48e392588679bf9825c10d9311710476dd96f173
0b1kn00b <polysemantic@gmail.com>
2021-06-10 09:41:50 +0200

    Fetch needs asClientDef

--- src/main/haxe/stx/http/client/Fetch.hx
+++ src/main/haxe/stx/http/client/Fetch.hx
@@ -11,4 +11,7 @@ abstract class Fetch<C:FetchConfigDef,P,R,E> implements ClientApi<P,R,E> extends
   public function request(method:HttpMethod,path:String,headers:Headers,body:Option<P>):Request{
     return Request.make(method,'${this.config.base}$path',this.config.options.headers.concat(headers),body.defv(null));
   }
+  public function asClientDef():ClienrDef<P,R,E>{
+    return this;
+  }
 }
