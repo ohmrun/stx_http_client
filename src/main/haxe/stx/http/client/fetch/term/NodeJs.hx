b36b8dc25bf0b833e2bec6e4f9cee408aa09f5a6
0b1kn00b <polysemantic@gmail.com>
2022-05-06 13:00:51 +0200

    mumble mumble

--- src/main/haxe/stx/http/client/fetch/term/NodeJs.hx
+++ src/main/haxe/stx/http/client/fetch/term/NodeJs.hx
@@ -11,20 +11,33 @@ package stx.http.client.fetch.term;
     public function defer(state:RemotingPayload<Noise>,cont:Terminal<RemotingPayload<Noise>,Noise>):Work{
       __.log().debug('request');
       final request = state.asset.request.toNodeFetchRequest();
+      final result  = NodeFetch.default_(request);
       return cont.receive(
         cont.later(
-          Pledge.fromJsPromise(NodeFetch.default_(request)).fold(
+          Pledge.fromJsPromise(result)
+          .fold(
             ok -> 
               state.mapi(
                 context -> RemotingContext.make(
                   context.request,
-                  Some(Response.fromNodeFetchResponse(__.tracer()(ok)))
+                  Some(Response.fromNodeFetchResponse(ok))
                 )
               ),
-            no -> state.defect( 
-              no.errate(E_HttpClient_Unknown)
-            ) 
-          ).map(__.success)
+            no -> 
+            {
+              return state.defect(
+                switch(no.data){
+                  case Some(INTERIOR(x)) : __.fault().decline(INTERIOR(x));
+                  case Some(EXTERIOR(x)) : no.errate(E_HttpClient_Unknown);
+                  case None              : __.fault().explain(_ -> _.e_undefined());
+                } 
+              );
+            }            
+          ).map(
+            x -> {
+              __.log().debug('$x');
+              return __.success(x);
+          })
         )
       );
     }
